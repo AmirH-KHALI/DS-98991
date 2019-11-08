28749e8f7667b7f9a5185a299e1943ed4cdfe046
AmirH Ahmadi <amirh.khali@gmail.com>
2019-11-08 14:22:48 +0330

    A6 finished

---
+++ A6/A6/Q1MoneyChange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A6
+{
+    public class Q1MoneyChange: Processor
+    {
+        private static readonly int[] COINS = new int[] {1, 3, 4};
+
+        public Q1MoneyChange(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long>) Solve);
+
+        public long Solve(long n)
+        {
+            long[] dp = new long[n + 1];
+            for (int i = 1; i <= n; ++i) {
+                dp[i] = long.MaxValue;
+                if (i >= 1) dp[i] = Math.Min(dp[i], dp[i - 1] + 1);
+                if (i >= 3) dp[i] = Math.Min(dp[i], dp[i - 3] + 1);
+                if (i >= 4) dp[i] = Math.Min(dp[i], dp[i - 4] + 1);
+            }
+            return dp[n];
+        }
+    }
+}
---
+++ A6/A6/Q5LCSOfThree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace A6
+{
+    public class Q5LCSOfThree: Processor
+    {
+        public Q5LCSOfThree(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[], long[], long[], long>)Solve);
+
+        public long Solve(long[] str1, long[] str2, long[] str3)
+        {
+            long[, , ] dp = new long[str1.Length + 1, str2.Length + 1, str3.Length + 1];
+            dp[0, 0, 0] = 0;
+            for (int i = 0; i <= str1.Length; ++i) {
+                for (int j = 0; j <= str2.Length; ++j) {
+                    for (int k = 0; k <= str3.Length; ++k) {
+                        dp[i, j, k] = 0;
+                        if (i > 0) dp[i, j, k] = Math.Max(dp[i, j, k], dp[i - 1, j, k]);
+                        if (j > 0) dp[i, j, k] = Math.Max(dp[i, j, k], dp[i, j - 1, k]);
+                        if (k > 0) dp[i, j, k] = Math.Max(dp[i, j, k], dp[i, j, k - 1]);
+                        if (i > 0 && j > 0 && k > 0 && str1[i - 1] == str2[j - 1] && str2[j - 1] == str3[k - 1]) {
+                            dp[i, j, k] = Math.Max(dp[i, j, k], dp[i - 1, j - 1, k - 1] + 1);
+                        }
+                    }
+                }
+            }
+            return dp[str1.Length, str2.Length, str3.Length];
+        }
+    }
+}
