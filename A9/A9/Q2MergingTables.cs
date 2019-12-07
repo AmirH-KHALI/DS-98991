fda80b4b2a1053109faaa7853e2ca0e8fd2df477
AmirH Ahmadi <amirh.khali@gmail.com>
2019-12-07 23:25:47 +0330

    A9 1, 2, 4 completed

---
+++ A9/A9/Q2MergingTables.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using TestCommon;
+
+namespace A9
+{
+    public class Q2MergingTables : Processor
+    {
+        long[] parent;
+        long[] tableSizes;
+        long[] rank;
+
+        public Q2MergingTables(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[], long[], long[], long[]>)Solve);
+
+
+        public long[] Solve(long[] tableSizes, long[] targetTables, long[] sourceTables)
+        {
+            long[] par = new long[tableSizes.Length];
+
+            long[] ans = new long[targetTables.Length];
+
+            for (int i = 0; i < tableSizes.Length; ++i) {
+                par[i] = i;
+            }
+
+            long mx = Int64.MinValue;
+
+            for (int i = 0; i < tableSizes.Length; ++i) {
+                mx = Math.Max(mx, tableSizes[i]);
+            }
+
+            for (int i = 0; i < targetTables.Length; ++i) {
+                targetTables[i] = find(targetTables[i], par);
+                sourceTables[i] = find(sourceTables[i], par);
+                if (targetTables[i] != sourceTables[i]) {
+                    tableSizes[targetTables[i] - 1] += tableSizes[sourceTables[i] - 1];
+                    par[sourceTables[i]] = targetTables[i];
+                }
+                ans[i] = Math.Max(tableSizes[targetTables[i] - 1], mx);
+                mx = ans[i];
+            }
+            return ans;
+        }
+
+        private long find(long v, long[] par) {
+            if (par[v] == v) 
+                return v;
+            return par[v] = find(par[v], par);
+        }
+
+    }
+}
---
+++ A9/A9/Q4ParallelProcessing.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A9
+{
+    public class Q4ParallelProcessing : Processor
+    {
+        public Q4ParallelProcessing(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], Tuple<long, long>[]>)Solve);
+
+        public Tuple<long, long>[] Solve(long threadCount, long[] jobDuration)
+        {
+            List<Tuple<long, long>> heap = new List<Tuple<long, long>>();
+
+            List<Tuple<long, long>> ans = new List<Tuple<long, long>>();
+
+            //*
+            for (int i = 0; i < threadCount; ++i) {
+                if (i == 0) heap.Add(new Tuple<long, long>(i, 0));
+                else heap.Add(new Tuple<long, long>(threadCount - i, 0));
+            }
+
+            for (int i = 0; i < jobDuration.Length; ++i) {
+                Tuple<long, long> mn = extMin(heap);
+                ans.Add(mn);
+                Tuple<long, long> newThread = new Tuple<long, long>(mn.Item1, mn.Item2 + jobDuration[i]);
+            }
+            /*/
+            for (int i = 0; i < threadCount; ++i) {
+                ans.Add(new Tuple<long, long>(i, 0));
+            }
+            //*/
+
+            return ans.ToArray();
+        }
+
+        private Tuple<long, long> extMin(List<Tuple<long, long>> heap) {
+            Tuple<long, long> ans = heap[0];
+            (heap[0], heap[heap.Count - 1]) = (heap[heap.Count - 1], heap[0]);
+            heap.RemoveAt(heap.Count - 1);
+            siftDown(0, heap);
+            return ans;
+        }
+
+        private void siftDown(int i, List<Tuple<long, long>> heap) {
+            while (true) {
+                int min = i;
+                int left  = 2 * i + 1;
+                int right = 2 * i + 2;
+                if (left  < heap.Count && comp(heap[left] , heap[min])) {
+                    min = left;
+                }
+                if (right < heap.Count && comp(heap[right], heap[min])) {
+                    min = right;
+                }
+                if (min == i) {
+                    break;
+                }
+                (heap[i], heap[min]) = (heap[min], heap[i]);
+                i = min;
+            }
+        }
+
+        private bool comp(Tuple<long, long> tuple1, Tuple<long, long> tuple2) {
+            return tuple1.Item2 < tuple2.Item2;
+        }
+    }
+}
